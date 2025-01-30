e6a85c97c90d0c2a74577cde6f2f11e9132ec84c
Lara Oliveira <lara.oliveira@zup.com.br>
2025-01-30 13:37:23 -0300

    adiciona a classe Client

---
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/SistemaECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.zup.ECommerce.Sistema.E_Commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/ECommerce/Sistema/E_Commerce/SistemaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.ECommerce.Sistema.E_Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
