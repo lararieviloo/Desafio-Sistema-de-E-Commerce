1a3f37d42746cfd6985ad0aeabcb4f3af86d4e1f
Lara Oliveira <lara.oliveira@zup.com.br>
2025-01-31 14:49:15 -0300

    adiciona as validações nas classes ClientRequestDTO e ProductRequestDTO

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ClientRequestDTO.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ClientRequestDTO.java
@@ -1,4 +1,13 @@
 package com.zup.ECommerce.Sistema.E_Commerce.dtos;
 
-public record ClientRequestDTO() {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record ClientRequestDTO(
+        @NotBlank(message = "O nome não pode ser em branco.") String clientName,
+
+        @CPF(message = "O CPF deve ser válido.") String cpf,
+
+        @Email(message = "O Email deve ser válido.") String email)
+{}
--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ProductRequestDTO.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ProductRequestDTO.java
@@ -1,4 +1,11 @@
 package com.zup.ECommerce.Sistema.E_Commerce.dtos;
 
-public record ProductRequestDTO() {
-}
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+public record ProductRequestDTO(
+        @NotBlank(message = "O nome do produto não pode ser em branco.") String productName,
+        @Positive(message = "O preço deve ser maior que zero.") double price,
+        @Min(value = 0, message = "A quantidade deve ser maior ou igual a zero.") int quantity)
+{}
