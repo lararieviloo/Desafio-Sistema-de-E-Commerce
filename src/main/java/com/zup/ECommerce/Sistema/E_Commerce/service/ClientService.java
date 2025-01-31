67d95b77399db9f2b4074a0ff15b20951afc00bc
Lara Oliveira <lara.oliveira@zup.com.br>
2025-01-31 15:18:47 -0300

    adiciona métodos para adicionar um novo cliente, buscar um cliente pelo CPF e atualizar as informações de um cliente pelo CPF

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/service/ClientService.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/service/ClientService.java
@@ -1,5 +1,11 @@
 package com.zup.ECommerce.Sistema.E_Commerce.service;
 
+import com.zup.ECommerce.Sistema.E_Commerce.models.Client;
+import com.zup.ECommerce.Sistema.E_Commerce.dtos.ClientRequestDTO;
+
 
 public interface ClientService {
+    Client addClient(ClientRequestDTO clientRequestDTO);
+    Client getClientByCpf(String cpf);
+    Client updateClient(String cpf, ClientRequestDTO clientRequestDTO);
 }
