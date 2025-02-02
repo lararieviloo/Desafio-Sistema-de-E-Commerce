930e8b23dd21d951e6d483e3fb8bb6178e96da99
Lara Oliveira <lara.oliveira@zup.com.br>
2025-02-02 13:57:18 -0300

    cria a classe ProductResponseDTO

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ProductResponseDTO.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ProductResponseDTO.java
@@ -1,4 +1,50 @@
 package com.zup.ECommerce.Sistema.E_Commerce.dtos;
 
 public class ProductResponseDTO {
+
+    private Long id;
+    private String productName;
+    private double price;
+    private int quantity;
+
+    public ProductResponseDTO() {}
+
+    public ProductResponseDTO(Long id, String productName, double price, int quantity) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
