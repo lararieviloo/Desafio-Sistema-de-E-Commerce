fa99e970b48420d0c750f425fb73dd000a20df68
Lara Oliveira <lara.oliveira@zup.com.br>
2025-02-03 16:06:02 -0300

    adiciona um método que busca um cliente no bancos de dados pelo CPF em ClientRepository

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/repository/ClientRepository.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/repository/ClientRepository.java
@@ -3,7 +3,10 @@ package com.zup.ECommerce.Sistema.E_Commerce.repository;
 import com.zup.ECommerce.Sistema.E_Commerce.models.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<ClientEntity, String> {
     boolean existsByCpf(String cpf);
     boolean existsByEmail(String email);
+    Optional<ClientEntity> findByCpf(String cpf);
 }
