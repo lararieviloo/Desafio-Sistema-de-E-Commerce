cde0f4501a1487da60019b06007ef635d3bf0d1a
Lara Oliveira <lara.oliveira@zup.com.br>
2025-02-05 16:14:11 -0300

    adiciona a exceção MethodArgumentNotValidException na classe GlobalExceptionHandler para receber a mensagem de erro de cpf e email inválidos corretamente no endpoint

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/exceptions/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.zup.ECommerce.Sistema.E_Commerce.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
 
     @ExceptionHandler(ClientAlreadyExistsException.class)
     public ResponseEntity<String> handleClientAlreadyExistsException(ClientAlreadyExistsException ex) {
@@ -26,6 +28,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Um erro inesperado aconteceu: " + ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
     }
 
     @ExceptionHandler(Exception.class)
