a0dcee19962a3eda6d3a7411387b86cf2e33bf9f
Lara Oliveira <lara.oliveira@zup.com.br>
2025-02-04 22:12:07 -0300

    ajusta o retorno dos métodos que estavam errados (Client para ClientResponseDTO)

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/service/ClientService.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/service/ClientService.java
@@ -1,11 +1,11 @@
 package com.zup.ECommerce.Sistema.E_Commerce.service;
 
-import com.zup.ECommerce.Sistema.E_Commerce.models.Client;
+import com.zup.ECommerce.Sistema.E_Commerce.dtos.ClientResponseDTO;
 import com.zup.ECommerce.Sistema.E_Commerce.dtos.ClientRequestDTO;
 
 
 public interface ClientService {
-    Client addClient(ClientRequestDTO clientRequestDTO);
-    Client getClientByCpf(String cpf);
-    Client updateClient(String cpf, ClientRequestDTO clientRequestDTO);
+    ClientResponseDTO addClient(ClientRequestDTO clientRequestDTO);
+    ClientResponseDTO getClientByCpf(String cpf);
+    ClientResponseDTO updateClient(String cpf, ClientRequestDTO clientRequestDTO);
 }
