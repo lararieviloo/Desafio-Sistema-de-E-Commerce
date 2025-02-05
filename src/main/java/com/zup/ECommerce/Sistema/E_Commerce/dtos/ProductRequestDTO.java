fdde44064ce26cf37ba980a97ab7e70d6c273c31
Lara Oliveira <lara.oliveira@zup.com.br>
2025-02-05 00:50:19 -0300

    atualiza a classe ProductRequestDTO para ser uma classe record

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ProductRequestDTO.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ProductRequestDTO.java
@@ -4,45 +4,10 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
-public class ProductRequestDTO {
-        @NotBlank(message = "O nome do produto não pode ser em branco.")
-        private String productName;
+public record ProductRequestDTO(
+        @NotBlank(message = "O nome do produto não pode ser em branco.") String productName,
 
-        @Positive(message = "O preço deve ser maior que zero.")
-        private double price;
+        @Positive(message = "O preço deve ser maior que zero.") double price,
 
-        @Min(value = 0, message = "A quantidade deve ser maior ou igual a zero.")
-        private int quantity;
-
-    public ProductRequestDTO() {}
-
-    public ProductRequestDTO(String productName, double price, int quantity) {
-        this.productName= productName;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductname(String productName) {
-        this.productName = productName;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-}
+        @Min(value = 0, message = "A quantidade deve ser maior ou igual a zero.") int quantity)
+{}
