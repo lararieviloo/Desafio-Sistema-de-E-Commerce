af035119c61ea4ce914643643285546562cc7fdb
Lara Oliveira <lara.oliveira@zup.com.br>
2025-02-04 21:32:11 -0300

    cria a classe ClientServiceImpl e ajusta ClientRequestDTO para ser uma classe record

--- src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ClientRequestDTO.java
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/dtos/ClientRequestDTO.java
@@ -4,45 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.br.CPF;
 
-public class ClientRequestDTO {
-        @NotBlank(message = "O nome não pode ser em branco.")
-        private String clientName;
+public record ClientRequestDTO(
+        @NotBlank(message = "O nome não pode ser em branco.") String clientName,
 
-        @CPF(message = "O CPF deve ser válido.")
-        private String cpf;
+        @CPF(message = "O CPF deve ser válido.") String cpf,
 
-        @Email(message = "O Email deve ser válido.")
-         private String email;
-
-        public ClientRequestDTO() {}
-
-    public ClientRequestDTO(String clientName, String cpf, String email) {
-        this.clientName = clientName;
-        this.cpf = cpf;
-        this.email = email;
-    }
-
-    public String getClientName() {
-        return clientName;
-    }
-
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
+        @Email(message = "O Email deve ser válido.") String email
+)
+{}
---
+++ src/main/java/com/zup/ECommerce/Sistema/E_Commerce/service/ClientServiceImpl.java
@@ -0,0 +1,65 @@
+package com.zup.ECommerce.Sistema.E_Commerce.service;
+
+
+import com.zup.ECommerce.Sistema.E_Commerce.dtos.ClientRequestDTO;
+import com.zup.ECommerce.Sistema.E_Commerce.dtos.ClientResponseDTO;
+import com.zup.ECommerce.Sistema.E_Commerce.models.ClientEntity;
+import com.zup.ECommerce.Sistema.E_Commerce.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+
+    private ClientEntity mapToEntity(ClientRequestDTO clientRequestDTO) {
+
+        return new ClientEntity(clientRequestDTO.clientName(),
+                clientRequestDTO.cpf(),
+                clientRequestDTO.email());
+    }
+
+    private ClientResponseDTO mapToResponse(ClientEntity clientEntity) {
+        return new ClientResponseDTO(clientEntity.getId(),
+                clientEntity.getClientName(),
+                clientEntity.getCpf(),
+                clientEntity.getEmail());
+    }
+    @Override
+    public ClientResponseDTO addClient(ClientRequestDTO clientRequestDTO) {
+        String cpf = clientRequestDTO.cpf();
+        if (clientRepository.existsByCpf(cpf)) {
+            throw new ClientAlreadyExistsException("O CPF informado já existe.");
+        }
+
+        String email = clientRequestDTO.email();
+        if (clientRepository.existsByEmail(email)) {
+            throw new ClientAlreadyExistsException("O email informado já existe.");
+        }
+
+        ClientEntity clientEntity = mapToEntity(clientRequestDTO);
+        clientRepository.save(clientEntity);
+        return mapToResponse(clientEntity);
+    }
+
+    @Override
+    public ClientResponseDTO getClientByCpf(String cpf) {
+        ClientEntity clientEntity = clientRepository.findByCpf(cpf)
+                .orElseThrow(() -> new ClientNotFoundException("Não foi possível achar o cliente com o CPF informado."));
+        return mapToResponse(clientEntity);
+    }
+
+    @Override
+    public ClientResponseDTO updateClient(String cpf, ClientRequestDTO clientRequestDTO) {
+        ClientEntity clientEntity = clientRepository.findByCpf(cpf)
+                .orElseThrow(() -> new ClientNotFoundException("Não foi possível achar o cliente com o CPF informado."));
+        clientEntity.setClientName(clientRequestDTO.clientName());
+        clientEntity.setCpf(clientRequestDTO.cpf());
+        clientEntity.setEmail(clientRequestDTO.email());
+        clientRepository.save(clientEntity);
+        return mapToResponse(clientEntity);
+    }
+}
